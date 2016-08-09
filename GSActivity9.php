f41ec3e4c2febf3299e6d02bfba9e497108eb6e7
kavenGw <371905989@qq.com>
2016-08-09 17:01:18 +0800

    增加5个启动文件

---
+++ GSActivity9.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define("ROOT", __DIR__);
+define("RoleStartIndex",8000);
+define("RoleCount",200);
+require_once(ROOT."/GSClient.php");
